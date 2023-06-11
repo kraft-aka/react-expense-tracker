e0f74fdc47d6f464a860784f8375949884cefba1
kubat <kubat.aiupov@gmail.com>
2023-06-11 23:06:31 +0200

    add state to ExpenseFrom

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,21 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 export default function ExpenseForm() {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  function titleChangeHandler(e) {
+    setEnteredTitle(e.target.value);
+  }
+
+  function amountChangeHandler(e) {
+    setEnteredAmount(e.target.value);
+  }
+
+  function dateChangeHandler(e) {
+    setEnteredDate(e.target.value);
+  }
+
   return (
     <form action="">
       <div className="new-expense__constrols">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-31" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
