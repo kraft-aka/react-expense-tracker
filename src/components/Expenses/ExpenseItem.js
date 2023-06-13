cb63c3d982658b7e9719655c8edf76d26223a563
kubat <kubat.aiupov@gmail.com>
2023-06-13 22:39:18 +0200

    clean up ExpenseItem comp

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,22 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
-
-  const [title, setTitle] = useState(props.title);
-
-  function clickHandler() {
-    setTitle('Updated');
-    console.log(title);
-
-  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
