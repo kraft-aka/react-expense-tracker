c92fdc141c2d3b8524d7fc7e6b6f6ecfbfe8d365
kubat <kubat.aiupov@gmail.com>
2023-06-13 22:37:24 +0200

    init ExpensesFilter comp

---
+++ src/components/NewExpense/ExpensesFilter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function ExpensesFilter() {
+  return (
+    <form action="">
+      <label>Filter by Year</label>
+      <select name="" id="">
+        <option value="2019">2019</option>
+        <option value="2020">2020</option>
+        <option value="2021">2021</option>
+        <option value="2022">2022</option>
+      </select>
+    </form>
+  )
+}
